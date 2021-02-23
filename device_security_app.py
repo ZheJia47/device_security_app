87dc798a98206047f0fd5d297bdb60bb0a6a4595
ZheJia47 <53133006+ZheJia47@users.noreply.github.com>
2021-02-23 10:33:28 +0800

    basic function built with python

--- device_security_app.py
+++ device_security_app.py
@@ -2,8 +2,29 @@ import os
 import time
 
 # connect the bluetooth of cell ##################################
-my_phone_MAC = '58-cb-52-9a-df-60' # the MAC address of the cell 
-os.system('sh device_security_app.sh %s' % my_phone_MAC)
+os.system('blueutil --paired')
+print()
+my_phone_MAC = input('please type the MAC address of your device: ')
+# my_phone_MAC = '58-cb-52-9a-df-60' # the MAC address of the cell 
+isConnected = os.popen('echo "$(blueutil --is-connected %s)"' % my_phone_MAC).read()
+
+while True:
+    # first connect
+    if '0' in isConnected:
+        os.system('blueutil --connect %s' % my_phone_MAC)  
+        isConnected = os.popen('echo "$(blueutil --is-connected %s)"' % my_phone_MAC).read()     
+        # second check
+        if '0' in isConnected:
+            os.system('pmset displaysleepnow')            
+        else:
+            print('device %s is connected' % my_phone_MAC)
+    else:
+        print('device %s is already connected' % my_phone_MAC)
+        
+    time.sleep(3)
+    isConnected = os.popen('echo "$(blueutil --is-connected %s)"' % my_phone_MAC).read()
+
+
 
 
 
